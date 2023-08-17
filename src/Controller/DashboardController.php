3c44f4981281784489403b0770fb0c9b76c30ec0
BattutDev <antoine@battut.dev>
2023-08-17 17:18:17 +0200

    fix: improve ux

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,22 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'dashboard.index', methods: ['GET'])]
-    public function index(): Response
+    public function index(#[CurrentUser] User $user): Response
     {
         return $this->render('dashboard/index.html.twig',
-
             [
                 "breadcrumbs" => [
                     "paths" => [['Dashboard', '/dashboard/']],
                     "page" => 'Acceuil'
-                ]
+                ],
+                "user" => $user
             ]
         );
     }
