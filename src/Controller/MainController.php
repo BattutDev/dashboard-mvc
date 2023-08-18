975aa26693c0e8405fb8dfd6521de0a8cf6d2537
BattutDev <antoine@battut.dev>
2023-08-18 12:25:33 +0200

    feat: identification failed error

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,12 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main.index', methods: ['GET'])]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('index.html.twig');
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 }
